adc0a20ba59f8744c72a6a15968b6738f1940b78
anasybal <59410395+anasybal@users.noreply.github.com>
2020-06-01 11:43:15 +0200

    Add files via upload

---
+++ info.php
@@ -0,0 +1,13 @@
+<?php
+// Theme Name
+$name = 'cuba';
+
+// Theme Author
+$author = 'croco-ly';
+
+// Theme URL
+$url = 'https://www.instqram.ml/@anasybal';
+
+// Theme Version
+$version = '1.0.0';
+?>
